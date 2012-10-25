f9bec6b7c30dfca1e2f2789f72b8f540e1a7dfa8
Mike Frey <mike.frey@tstmedia.com>
2012-10-24 19:50:51 -0500

    added session plugin tests. Moved session code to a module

---
+++ test/session_test.js
@@ -0,0 +1,28 @@
+var assert = require('assert')
+var sinon = require('sinon')
+var ControllerFactory = require('./fixtures/controller')
+var Session = require('../plugins/session')
+
+var Controller
+var instance
+
+describe('Session', function() {
+
+  beforeEach(function(done) {
+    Controller = ControllerFactory.create()
+    Controller.addPlugin(Session)
+    instance = ControllerFactory.init(Controller)
+
+    done()
+  })
+
+  describe('Exists', function() {
+
+    it('should add session object to controller', function(done) {
+      assert(instance.session)
+      done()
+    })
+
+  })
+
+})
